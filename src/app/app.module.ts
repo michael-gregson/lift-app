5666e61db7edeef381feabeb1b43c75be00c7d28
Michael Gregson <michael.gregson@utilitywise.com>
2017-08-11 15:11:06 +0100

    Install hammerjs for gesture support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import 'hammerjs';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
